0b3a2897df1f9e33389bba364f538dbc52d9ee2c
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-03-18 00:43:48 +0530

    feat : index files with necessary imports and exports

---
+++ src/assets/icons/index.js
@@ -0,0 +1,12 @@
+import bag from './bag.png'
+import blackBag from './bag-black.png'
+import exchange from './exchange.png'
+import heart from './heart.png'
+import home from './home.png'
+import pbLogo from './pb-logo.png'
+import secure from './secure.png'
+import support from './support.png'
+import truck from './truck.png'
+import user from './user.png'
+
+export { bag, blackBag, exchange, heart, home, pbLogo, secure, support, truck, user }
---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+import bird from './bird.png'
+import bunny from './bunny.png'
+import cat from './cat.png'
+import dog from './dog.png'
+import fish from './fish.png'
+import heroImage from './p&b_banner.png'
+
+export { bird, bunny, cat, dog, fish, heroImage }
