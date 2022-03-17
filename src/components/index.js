78a48e6c5d468717d88571554de931f7c4f03cb5
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-03-18 00:44:19 +0530

    refactor : remaining files added

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import { Categories } from './categories/categories'
+import { Deals } from './deals/deals'
+import { Footer } from './footer/footer'
+import { Header } from './header/header'
+import { HeroImage } from './hero_image/heroImage'
+import { WhyUsSection } from './why_us/whyUs'
+import { ProductCard } from './card/productCard/productCard'
+import { Trending } from './trending/trending'
+
+export { Categories, Deals, Footer, Header, HeroImage, WhyUsSection, ProductCard, Trending }
