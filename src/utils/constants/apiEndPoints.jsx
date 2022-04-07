2e13a13eb2593824c1e9be749adbd2ed13eb75a7
samyukthasudhakar <samyuktha.sudhakar@gmail.com>
2022-04-07 14:50:21 +0530

    refactor : removed semicolons from constant declaratios

--- src/utils/constants/apiEndPoints.jsx
+++ src/utils/constants/apiEndPoints.jsx
@@ -1,9 +1,9 @@
-const SIGNUP_API = "/api/auth/signup";
-const LOGIN_API = "/api/auth/login";
-const GET_PRODUCTS = "/api/products";
-const GET_CATEGORIES = "/api/categories";
-const ADD_TO_WISHLIST = "/api/user/wishlist";
-const DELETE_FROM_WISHLIST = "/api/user/wishlist/";
+const SIGNUP_API = "/api/auth/signup"
+const LOGIN_API = "/api/auth/login"
+const GET_PRODUCTS = "/api/products"
+const GET_CATEGORIES = "/api/categories"
+const ADD_TO_WISHLIST = "/api/user/wishlist"
+const DELETE_FROM_WISHLIST = "/api/user/wishlist/"
 
 export { 
 SIGNUP_API, 
